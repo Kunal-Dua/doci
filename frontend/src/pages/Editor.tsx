7548f9a0058a856e407bba7ffe586fcb1348ac89
Kunal Dua <kunaldua2000@gmail.com>
2026-03-02 04:42:47 +0530

    added cursor

--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -3,9 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import ReactQuill from "react-quill-new";
 import { useLocation } from "react-router-dom";
 import "quill/dist/quill.snow.css";
-import type { Delta } from "quill";
-
-type Sources = "user" | "api" | "silent";
+import { Delta, type EmitterSource } from "quill";
 
 const Editor = () => {
   const location = useLocation();
@@ -15,7 +13,7 @@ const Editor = () => {
   const quillRef = useRef<ReactQuill | null>(null);
 
   async function callWS() {
-    await axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/doc/${docId}`);
+    const res = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/v1/doc/d/${docId}`);
   }
 
   // useEffect(() => {
@@ -31,16 +29,23 @@ const Editor = () => {
       if (!quillRef.current) return;
       const data = JSON.parse(event.data);
       const editor = quillRef.current.getEditor();
-      
-      if (data.type === "load-document") {
-        console.log(data.delta);
 
+      if (data.type === "load-document") {
         editor.setContents(data.delta);
         // setData(m => m + data.delta);
       }
 
       if (data.type === "receive-changes") {
-        editor.updateContents(data.delta);
+        const range = editor.getSelection();
+        const delta = new Delta(data.delta);
+
+        editor.updateContents(delta);
+
+        if (range) {
+          const newIndex = delta.transformPosition(range.index);
+          editor.setSelection(newIndex, range.length);
+        }
+        // editor.updateContents(data.delta);
         // setData(m => m + data.delta);
       }
     };
@@ -59,16 +64,22 @@ const Editor = () => {
     };
   }, []);
 
-  const handleChange = (value: string, delta: Delta, source: Sources) => {
+  const handleChange = (
+    value: string,
+    delta: Delta,
+    source: EmitterSource,
+    editor: ReactQuill.UnprivilegedEditor
+  ) => {
     setData(value);
 
     const ws = wsRef.current;
-
+    const cursor = editor.getSelection();
     if (ws && ws.readyState === WebSocket.OPEN && source === "user") {
       ws.send(
         JSON.stringify({
           type: "send-changes",
           delta,
+          cursor,
         })
       );
     }
